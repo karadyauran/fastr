488ba05a7be0a77ecf354e80e332d6a7bf545457
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-04-16 22:55:34 +0200

    update migrations script

--- scripts/do_all_migrations.py
+++ scripts/do_all_migrations.py
@@ -2,9 +2,13 @@ import os
 import subprocess
 from colorama import Fore, Style
 
-down_arrow = '\u2193'
-check_mark = '\u2713'
-cross_mark = '\u2717'
+signals = {
+    'down_arrow': '\u2193',
+    'check_mark': '\u2713',
+    'cross_mark': '\u2717',
+}
+
+files_black_list = ['config', 'manage.py', 'db.sqlite3', '__pycache__']
 
 microservices = [
     'authentication',
@@ -17,38 +21,31 @@ microservices = [
 ]
 
 
-def success_exit(msg, name) -> None:
-    print(Style.BRIGHT + Fore.GREEN + check_mark + ' Success ' + Style.RESET_ALL + f'{msg} {name}' + Style.RESET_ALL)
+def exit_message(error: bool, msg: str, name: str) -> str:
+    color = Fore.RED if not error else Fore.GREEN
+    status = f'{signals['cross_mark']} FAILED' if not error else f'{signals['check_mark']} SUCCESS'
+    style = Style.BRIGHT
+    reset = Style.RESET_ALL
+    return f'{color} {style} {status}: {reset} {msg} {name}'
 
 
-def fail_exit(msg, name) -> None:
-    print(Style.BRIGHT + Fore.RED + cross_mark + ' Failed ' + Style.RESET_ALL + f'{msg} `{name}`' + Style.RESET_ALL)
+def make_migrations(file) -> str:
+    _ = subprocess.run(['python', 'manage.py', 'makemigrations', file])
+
+    if _.returncode == 0:
+        return exit_message(error=True, msg='Migration file created', name=file)
+
+    else:
+        raise Exception(exit_message(error=False, msg='Not created migration', name=file))
 
 
-def make_migrations(file) -> bool:
-    if file not in ['config', 'manage.py', 'db.sqlite3', '__pycache__']:
-        _ = subprocess.run(['python', 'manage.py', 'makemigrations', file])
-
-        if _.returncode == 0:
-            success_exit('to make migrations', file)
-
-        else:
-            fail_exit('to make migrations', file)
-            raise NameError(Style.BRIGHT + Fore.RED + 'Fail to make migrations' + Style.RESET_ALL)
-
-        return True
-
-
-def migrate(microservice) -> bool:
+def migrate(microservice) -> str:
     _ = subprocess.run(['python', 'manage.py', 'migrate'], capture_output=True, text=True)
 
     if 'No migrations to apply.' in _.stdout or _.returncode == 0:
-        success_exit('to migrate', microservice)
+        return exit_message(error=True, msg='Migrated', name=microservice)
     else:
-        fail_exit('to migrate', microservice)
-        raise Exception(_.stderr)
-
-    return True
+        raise Exception(exit_message(error=False, msg='Not migrated', name=microservice))
 
 
 def main():
@@ -57,13 +54,14 @@ def main():
     for microservice in microservices:
         os.chdir(microservice)
 
-        print(Fore.YELLOW + down_arrow + f' {microservice.capitalize()} service' + Style.RESET_ALL)
+        print(Fore.YELLOW + signals['down_arrow'] + f' {microservice.capitalize()} service' + Style.RESET_ALL)
         files = os.listdir()
 
         for file in files:
-            make_migrations(file)
+            if file not in files_black_list:
+                print(make_migrations(file))
 
-        migrate(microservice)
+        print(migrate(microservice))
 
         os.chdir('..')
 
