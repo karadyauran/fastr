7c52b2b19e3c0aa96ed53d7b05da0819d1921cda
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-04-17 10:09:55 +0200

    created test

--- authentication/authenticate/tests/__init__.py
+++ authentication/authenticate/tests/__init__.py
@@ -1,3 +1,3 @@
-from signup import *
-from login import *
-from logout import *
+from .signup import *
+from .login import *
+from .logout import *
--- authentication/config/settings.py
+++ authentication/config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from settings.base_settings import *
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('AUTHENTICATION_SECRET_KEY')
 DEBUG = True
 
